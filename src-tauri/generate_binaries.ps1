69d8fd7ade7bc57d9b848e0c85028bc9695bf357
Thomas Aunvik <contact@thaun.dev>
2024-08-25 13:28:48 +0200

    Fix Binary Gen

--- src-tauri/generate_binaries.ps1
+++ src-tauri/generate_binaries.ps1
@@ -14,33 +14,38 @@ if($IsWindows) {
 
 Copy-Item -Force "directstorage/*" "binaries/ff16pack"
 
+
 New-Item -Name tmp -ItemType "directory" -Force
-
-$nodeVersion = "v22.6.0"
-$nodeLinux = "node-$nodeVersion-linux-x64"
-
-if(![System.IO.File]::Exists("$nodeLinux.tar.xz")) {
-    Invoke-WebRequest -Uri "https://nodejs.org/dist/$nodeVersion/$nodeLinux.tar.xz" -OutFile "$nodeLinux.tar.xz"
-}
-if($IsWindows) {
-	tar -xf "${nodeLinux}.tar.xz" "**/bin/node"
-} else {
-	tar -xf "${nodeLinux}.tar.xz" --wildcards "**/bin/node"
-}
-
-# Windows
-$nodeWin = "node-$nodeVersion-win-x64"
-
-if(![System.IO.File]::Exists( "$nodeWin.zip")) {
-    Invoke-WebRequest -Uri "https://nodejs.org/dist/$nodeVersion/$nodeWin.zip" -OutFile "$nodeWin.zip"
-}
-
-tar -xf "$nodeWin.zip" "$nodeWin/node.exe"
+Set-Location tmp
 
 New-Item -Path "../binaries/" -Name "ffxvi-modmanager-runner" -ItemType "directory" -Force
 
-Copy-Item -Force "$nodeLinux/bin/node" "../binaries/ffxvi-modmanager-runner/ffxvi-modmanager-runner-x86_64-unknown-linux-gnu"
-Copy-Item -Force "$nodeWin/node.exe" "../binaries/ffxvi-modmanager-runner/ffxvi-modmanager-runner-x86_64-pc-windows-msvc.exe"
+$nodeVersion = "v22.6.0"
+
+if($IsLinux) {
+	$nodeLinux = "node-$nodeVersion-linux-x64"
+
+	if(![System.IO.File]::Exists("$nodeLinux.tar.xz")) {
+		Invoke-WebRequest -Uri "https://nodejs.org/dist/$nodeVersion/$nodeLinux.tar.xz" -OutFile "$nodeLinux.tar.xz"
+	}
+	
+	tar -xf "${nodeLinux}.tar.xz" --wildcards "**/bin/node"
+
+	Copy-Item -Force "$nodeLinux/bin/node" "../binaries/ffxvi-modmanager-runner/ffxvi-modmanager-runner-x86_64-unknown-linux-gnu"
+}
+
+if($IsWindows) {
+	# Windows
+	$nodeWin = "node-$nodeVersion-win-x64"
+
+	if(![System.IO.File]::Exists( "$nodeWin.zip")) {
+		Invoke-WebRequest -Uri "https://nodejs.org/dist/$nodeVersion/$nodeWin.zip" -OutFile "$nodeWin.zip"
+	}
+
+	tar -xf "$nodeWin.zip" "$nodeWin/node.exe"
+
+	Copy-Item -Force "$nodeWin/node.exe" "../binaries/ffxvi-modmanager-runner/ffxvi-modmanager-runner-x86_64-pc-windows-msvc.exe"
+}
 
 
 Set-Location ..
