3687ebfe7f10831d9308b2ea8bc2f9e451071666
Thomas Aunvik <contact@thaun.dev>
2024-08-25 13:19:02 +0200

    Use correct commands for making directories

--- src-tauri/generate_binaries.ps1
+++ src-tauri/generate_binaries.ps1
@@ -12,12 +12,9 @@ if($IsWindows) {
 	Rename-Item "binaries/ff16pack/FF16Tools.CLI" "FF16Tools.CLI"
 }
 
-
 Copy-Item -Force "directstorage/*" "binaries/ff16pack"
 
-
-mkdir tmp -ErrorAction SilentlyContinue
-Set-Location tmp
+New-Item -Name tmp -ItemType "directory" -Force
 
 $nodeVersion = "v22.6.0"
 $nodeLinux = "node-$nodeVersion-linux-x64"
@@ -38,10 +35,10 @@ if(![System.IO.File]::Exists( "$nodeWin.zip")) {
     Invoke-WebRequest -Uri "https://nodejs.org/dist/$nodeVersion/$nodeWin.zip" -OutFile "$nodeWin.zip"
 }
 
-
 tar -xf "$nodeWin.zip" "$nodeWin/node.exe"
 
-mkdir ../binaries/ffxvi-modmanager-runner -ErrorAction SilentlyContinue
+New-Item -Path "../binaries/" -Name "ffxvi-modmanager-runner" -ItemType "directory" -Force
+
 Copy-Item -Force "$nodeLinux/bin/node" "../binaries/ffxvi-modmanager-runner/ffxvi-modmanager-runner-x86_64-unknown-linux-gnu"
 Copy-Item -Force "$nodeWin/node.exe" "../binaries/ffxvi-modmanager-runner/ffxvi-modmanager-runner-x86_64-pc-windows-msvc.exe"
 
